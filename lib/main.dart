bb5b649926507f5ab3ace7854eb1a3fe7fe467a7
Edsson <akairu@hotmail.com>
2022-07-15 17:59:50 -0500

    icons class, materialpalette.com

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -49,6 +51,56 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
                     color: Colors.teal.shade100,
                     fontWeight: FontWeight.normal),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 20.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      '+52 4431880XXX',
+                      style: TextStyle(
+                          fontFamily: 'Exo',
+                          fontSize: 15.0,
+                          letterSpacing: 2.5,
+                          color: Colors.teal.shade900,
+                          fontWeight: FontWeight.normal),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Icon(
+                      Icons.email,
+                      size: 20.0,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'akairu@hotmail.com',
+                      style: TextStyle(
+                          fontFamily: 'Exo',
+                          fontSize: 15.0,
+                          letterSpacing: 2.5,
+                          color: Colors.teal.shade900,
+                          fontWeight: FontWeight.normal),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
